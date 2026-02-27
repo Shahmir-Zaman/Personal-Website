1654fb900f3e45bb9dfe286e34930056eecf7e3d
Shahmir Zaman <94165328+Shahmir-Zaman@users.noreply.github.com>
2026-02-27 16:38:29 +0400

    feat: Bug fixes

--- src/components/Models/HeroModels/HeroExperience.jsx
+++ src/components/Models/HeroModels/HeroExperience.jsx
@@ -12,14 +12,14 @@ const HeroExperience = () => {
   const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
 
   return (
-    <Canvas camera={{ position: [5, 0, 42], fov: 50 }}>
+    <Canvas camera={{ position: [0, 0, 13], fov: 50 }}>
       {/* deep blue ambient */}
       <ambientLight intensity={0.2} color="#1a1a40" />
       {/* Configure OrbitControls to disable panning and control zoom based on device type */}
       <OrbitControls
         enablePan={false} // Prevents panning of the scene
         enableZoom={!isTablet} // Disables zoom on tablets
-        maxDistance={60} // Maximum distance for zooming out
+        maxDistance={75} // Maximum distance for zooming out
         minDistance={5} // Minimum distance for zooming in
         minPolarAngle={Math.PI / 5} // Minimum angle for vertical rotation
         maxPolarAngle={Math.PI / 2} // Maximum angle for vertical rotation
--- src/components/Models/HeroModels/HeroLights.jsx
+++ src/components/Models/HeroModels/HeroLights.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { PerspectiveCamera } from '@react-three/drei';
 
 const HeroLights = () => {
     return (
         <group>
-            <PerspectiveCamera makeDefault position={[0, 0, 10]} />
             <ambientLight intensity={0.5} />
             <directionalLight position={[10, 10, 10]} intensity={1} />
         </group>
